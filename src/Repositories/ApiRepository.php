2281f25e08429230d5e08058bebc2cacb1916dbf
lbee <congnv8192@gmail.com>
2017-09-15 14:04:33 +0700

    add ApiRepository

---
+++ src/Repositories/ApiRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace BiNet\Core\Repositories;
+
+class ApiRepository extends Repository {
+	protected $client;
+	protected $baseUri;
+
+	public function __construct() {
+		$this->client = new Client([
+			'base_uri' => $this->baseUri,
+			'timeout'  => 2.0
+		]);
+	}
+
+
+}
